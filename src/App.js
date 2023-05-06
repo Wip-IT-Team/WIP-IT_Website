895b37287cc3ec10a75921cc281585f19e9012a2
Kalyan <kalyankanuri497@gmail.com>
2023-05-06 21:23:55 +0530

    made changes to the website design and implemented search query

--- src/App.js
+++ src/App.js
@@ -1,71 +1,161 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import './App.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
+
+const projectsData = [
+  {
+    id: 1,
+    title: 'Project 1',
+    description: 'Description of Project 1',
+    image: '/images/project1.jpg',
+  },
+  {
+    id: 2,
+    title: 'Project 2',
+    description: 'Description of Project 2',
+    image: '/images/project2.jpg',
+  },
+  {
+    id: 3,
+    title: 'Project 3',
+    description: 'Description of Project 2',
+    image: '/images/project3.jpg',
+  },
+  // Add more project objects here
+];
+
+const teamData = [
+  {
+    id: 1,
+    name: 'Team Member 1',
+    description: 'Description of Team Member 1',
+    image: '/images/member1.jpg',
+  },
+  {
+    id: 2,
+    name: 'Team Member 2',
+    description: 'Description of Team Member 2',
+    image: '/images/member2.jpg',
+  },
+  {
+    id: 3,
+    name: 'Team Member 3',
+    description: 'Description of Team Member 2',
+    image: '/images/member3.jpg',
+  },
+  {
+    id: 2,
+    name: 'Team Member 4',
+    description: 'Description of Team Member 2',
+    image: '/images/member4.jpg',
+  },
+  {
+    id: 2,
+    name: 'Team Member 5',
+    description: 'Description of Team Member 2',
+    image: '/images/member5.jpg',
+  },
+  // Add more team member objects here
+];
+
 const App = () => {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [filteredProjects, setFilteredProjects] = useState(projectsData);
+  const [filteredTeam, setFilteredTeam] = useState(teamData);
+
+  const handleSearch = (event) => {
+    const query = event.target.value.toLowerCase();
+    setSearchQuery(query);
+
+    const filteredProjects = projectsData.filter((project) =>
+      project.title.toLowerCase().includes(query)
+    );
+    const filteredTeam = teamData.filter((member) =>
+      member.name.toLowerCase().includes(query)
+    );
+
+    setFilteredProjects(filteredProjects);
+    setFilteredTeam(filteredTeam);
+  };
+
+  const scrollRef = useRef(null);
+
+  const handleNavClick = (event, sectionId) => {
+    event.preventDefault();
+    const section = document.getElementById(sectionId);
+    if (section) {
+      setTimeout(() => {
+        section.scrollIntoView({ behavior: 'smooth' });
+      }, 500); // Delay in milliseconds
+    }
+  };
+
   return (
     <div className="App">
       <header>
-        <h1>Welcome to Our Website!</h1>
-      </header>
-      <nav>
-        <ul>
-          <li>
-            <a href="#home">Home</a>
-          </li>
-          <li>
-            <a href="#about">About</a>
-          </li>
-          <li>
-            <a href="#projects">Projects</a>
-          </li>
-          <li>
-            <a href="#team">Team</a>
-          </li>
-        </ul>
-      </nav>
-      <main>
-        <section id="home">
-          <div className="card">
-            <img src="/images/home-image.jpg" alt="Home" />
+        <h1>Welcome to Our Website! WIP-IT_Organization</h1>
+        <nav>
+          <ul>
+            <li>
+              <a href="#home">Home</a>
+            </li>
+            <li>
+              <a href="#about" onClick={(e) => handleNavClick(e, 'about')}>
+                About
+              </a>
+            </li>
+            <li>
+              <a href="#projects" onClick={(e) => handleNavClick(e, 'projects')}>
+                Projects
+              </a>
+            </li>
+            <li>
+              <a href="#team" onClick={(e) => handleNavClick(e, 'team')}>
+                Team
+              </a>
+            </li>
+          </ul>
+          <div className="search-bar">
+            <input
+              type="text"
+              placeholder="Search..."
+              value={searchQuery}
+              onChange={handleSearch}
+            />
+            <button type="submit">Search</button>
           </div>
-        </section>
-        <section id="about">
+        </nav>
+      </header>
+      <main>
+        <section id="about" ref={scrollRef}>
           <div className="card">
             <h2>About Us</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
           </div>
         </section>
-        <section id="projects">
+        <section id="projects" ref={scrollRef}>
           <h2>Our Projects</h2>
           <div className="card-grid">
-            <div className="card">
-              <img src="/images/project1.jpg" alt="Project 1" />
-              <h3>Project 1</h3>
-              <p>Description of Project 1</p>
-            </div>
-            <div className="card">
-              <img src="/images/project2.jpg" alt="Project 2" />
-              <h3>Project 2</h3>
-              <p>Description of Project 2</p>
-            </div>
-            {/* Add more project cards here */}
+            {filteredProjects.map((project) => (
+              <div className="card" key={project.id}>
+                <img src={project.image} alt={project.title} />
+                <h3>{project.title}</h3>
+                <p>{project.description}</p>
+              </div>
+            ))}
           </div>
         </section>
-        <section id="team">
+        <section id="team" ref={scrollRef}>
           <h2>Our Team</h2>
           <div className="card-grid">
-            <div className="card">
-              <img src="/images/member1.jpg" alt="Team Member 1" />
-              <h3>Team Member 1</h3>
-              <p>Description of Team Member 1</p>
-            </div>
-            <div className="card">
-              <img src="/images/member2.jpg" alt="Team Member 2" />
-              <h3>Team Member 2</h3>
-              <p>Description of Team Member 2</p>
-            </div>
-            {/* Add more team member cards here */}
+            {filteredTeam.map((member) => (
+              <div className="card" key={member.id}>
+                <img src={member.image} alt={member.name} />
+                <h3>{member.name}</h3>
+                <p>{member.description}</p>
+              </div>
+            ))}
           </div>
         </section>
       </main>
