66f369e76872d13b1f888c12f6d203bca7c85670
Kalyan <kalyankanuri497@gmail.com>
2023-05-22 15:00:06 +0530

    updating title

--- src/Components/exports.js
+++ src/Components/exports.js
@@ -1,4 +1,4 @@
-export const brandTitle = "Techbees";
+export const brandTitle = "TechBeez";
 // export const email ="aaradhyacargomovers@gmail.com";
 // export const contact = "9113855664 / 9743866386";
 // export const address = "#26, 1st Floor,Yeshwanthpura 1st Main Road, Mathikere, Opp. to Coffee Santhe Hotel, Bangalore, 560054"
